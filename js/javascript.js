297d6c5c348a09b81b3d1081c88c1d7bd7ae1395
albertng94 <albertng94@gmail.com>
2023-12-09 23:18:40 +0100

    Style interactive elements and add retract-expand functionality at sidebar.
    The divs that contain the different sidebar options are given a dark
    green background-color and solid border-left when hovered.
    
    The sidebar-handler "img-button" has been renamed to "sidebar-handler-
    retract", since it will be used to minimize the sidebar.
    A second flipped img-button called "sidebar-handler-expand" has been
    created and positioned where the sidebar remains when it is retracted.
    This second img-button is unseen by default and will be used to expand
    the sidebar when the conditions are met.
    
    Both buttons are given a rotation animation on hover.
    
    A retract-expand functionality has been added to the sidebar element
    through CSS and JS:
    - Function minimizeSidebar (later to be renamed) contains two event
    triggers.
    - When clicking the retract img-button, the header and main-content element are
    applied properties that change their base positioning on the grid,
    invading the second column and leaving the text part of the sidebar
    content unseen, while the icons remain visible. The expand button
    becomes visible as well.
    - When clicking the expand img-button, all the properties previously
    applied are removed, so the layout returns to the base state.

---
+++ js/javascript.js
@@ -0,0 +1,26 @@
+
+
+minimizeSidebar();
+
+
+function minimizeSidebar() {
+    const sidebarHandlerRetract = document.getElementById("sidebar-handler-retract");
+    const header = document.getElementById("header");
+    const mainContent = document.getElementById("main-content");
+    const titleDiv = document.getElementById("title-div");
+    const sidebarHandlerExpand = document.getElementById("sidebar-handler-expand");
+
+    sidebarHandlerRetract.addEventListener("click", () => {
+        header.classList.add("invadingHeader");
+        mainContent.classList.add("invadingMain-content");
+        titleDiv.classList.add("repositionLogo");
+        sidebarHandlerExpand.classList.add("expandHandler");
+    });
+
+    sidebarHandlerExpand.addEventListener("click", () => {
+        header.classList.remove("invadingHeader");
+        mainContent.classList.remove("invadingMain-content");
+        titleDiv.classList.remove("repositionLogo");
+        sidebarHandlerExpand.classList.remove("expandHandler");
+    });
+}
