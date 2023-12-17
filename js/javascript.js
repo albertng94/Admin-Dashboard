cc10ed7d81c2deeffea94dd4577b206bc5889eca
albertng94 <albertng94@gmail.com>
2023-12-17 13:56:54 +0100

    Add hover and click funcionality for the project icons
    Created "styleIcons" function on the js file. It targets all the projects
    icons and reacts on the following events:
    --> On mouseenter, the three icon outlines turn to green (the original
    img src is substituted by another one).
    --> On mouseleave, the three icons go back to the original image with
    the black outline, except the heart icon given it contains the class
    "clicked".
    --> On click, only the heart icon is affected. It turns and is completely
    filled up with the green colour, and adds the class "clicked". If the
    user clicks the heart when it already has the class "clicked" (when it
    is green), it turns again to the normal colour and removes the class
    "clicked".
    
    Links were added to some elements and href attributes completed for
    the project icon links.

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 
-
+//FUNCTION TO RESIZE THE SIDE-BAR ON CLICK
 resizeSidebar();
 
 
@@ -24,6 +24,66 @@ function resizeSidebar() {
         sidebarHandlerExpand.classList.remove("expandHandler");
     });
 }
+
+//FUNCTION TO STYLE PROJECT ICONS ON HOVER AND HEART ICON ON CLICK
+
+styleIcons();
+
+function styleIcons() {
+    const projectIcons = document.getElementsByClassName("project-icon");
+    const eyeImg = "../img/eye-outline-black.svg";
+    const greenEyeImg = "../img/eye-outline-green.svg";
+    const githubImg = "../img/github.svg";
+    const greenGithubImg = "../img/github-green.svg";
+    const heartImg = "../img/heart-outline-black.svg";
+    const heartBorderGreenImg = "../img/heart-outline-green.svg";
+    const heartTotalGreenImg = "../img/heart-total-green.svg";
+
+    for (const icon of projectIcons) {
+
+        icon.addEventListener("mouseenter", () => {
+            console.log(icon.className.includes("eye"));
+            console.log(icon.className);
+            if (icon.className.includes("eye")) {
+                console.log(icon);
+                icon.src = greenEyeImg;
+            } else if (icon.className.includes("github")) {
+                icon.src = greenGithubImg;
+            } else if (icon.className.includes("clicked")) {
+                icon.src = heartTotalGreenImg;
+            } else {
+                icon.src = heartBorderGreenImg;
+            }
+        }); 
+
+        icon.addEventListener("mouseleave", () => {
+            if (icon.className.includes("eye")) {
+                icon.src = eyeImg;
+            } else if (icon.className.includes("github")) {
+                icon.src = githubImg;
+            } else if (icon.className.includes("clicked")) {
+                icon.src = heartTotalGreenImg;
+            } else {
+                icon.src = heartImg;
+            }
+        });
+
+        icon.addEventListener("click", () => {
+            console.log(icon.classList["add"]);
+            console.log(icon.classList);
+            console.log(icon.className);
+            if (icon.className.includes("clicked")) {
+                icon.src = heartImg;
+                icon.classList.remove("clicked");
+            } else if (icon.id === "heart") {
+                icon.src = heartTotalGreenImg;
+                icon.classList.add("clicked");
+            }
+        });
+    }
+}
+
+
 
 // add shadows to header and main-content sections
 // make the font-sizes responsive
