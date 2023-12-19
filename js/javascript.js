7c119df5fa367079dda5115879e0bb9ea821a637
albertng94 <albertng94@gmail.com>
2023-12-19 20:12:50 +0100

    Add Dark/Light Mode functionality
    Created a Dark/Light mode functionality triggered by the checkbox
    (toggle):
    
    --> Colours for each mode are now stored in variables. The ones for
    lightmode are based in the :root element and the ones for the dark on
    the .dark class, which is add using JavaScript to the Body element.
    
    --> The dark mode uses is applied by adding the .dark class to the Body
    element when the toggle is clicked. It is also created in the localStorage
    so that when the user leaves the page on this mode, it will be kept the
    next time it is visited.
    
    --> Images need to be changed of colour in antoher way. New images with
    the wanted colour have been added to the img folder to be used for the
    dark mode. They are stored in variable and added as a new src in the place
    they belong, whenever body has the .dark class applied. Light mode images
    are also re-added, to make sure user can switch back dark mode and the
    images do so too.
    
    Some missing links have also been added to the header section.

--- js/javascript.js
+++ js/javascript.js
@@ -33,19 +33,19 @@ function styleIcons() {
     const projectIcons = document.getElementsByClassName("project-icon");
     const eyeImg = "../img/eye-outline-black.svg";
     const greenEyeImg = "../img/eye-outline-green.svg";
+    const whiteEyeImg = "../img/eye-outline-white.svg";
     const githubImg = "../img/github.svg";
     const greenGithubImg = "../img/github-green.svg";
+    const whiteGithubImg = "../img/github-white.svg";
     const heartImg = "../img/heart-outline-black.svg";
     const heartBorderGreenImg = "../img/heart-outline-green.svg";
     const heartTotalGreenImg = "../img/heart-total-green.svg";
+    const heartBorderWhiteImg = "../img/heart-outline-white.svg";
 
     for (const icon of projectIcons) {
 
         icon.addEventListener("mouseenter", () => {
-            console.log(icon.className.includes("eye"));
-            console.log(icon.className);
             if (icon.className.includes("eye")) {
-                console.log(icon);
                 icon.src = greenEyeImg;
             } else if (icon.className.includes("github")) {
                 icon.src = greenGithubImg;
@@ -58,13 +58,25 @@ function styleIcons() {
 
         icon.addEventListener("mouseleave", () => {
             if (icon.className.includes("eye")) {
-                icon.src = eyeImg;
+                if (document.body.className.includes("dark")) {
+                    icon.src = whiteEyeImg;
+                } else {
+                    icon.src = eyeImg;
+                }
             } else if (icon.className.includes("github")) {
-                icon.src = githubImg;
+                if (document.body.className.includes("dark")) {
+                    icon.src = whiteGithubImg;
+                } else {
+                    icon.src = githubImg;
+                }
             } else if (icon.className.includes("clicked")) {
                 icon.src = heartTotalGreenImg;
             } else {
-                icon.src = heartImg;
+                if (icon.id === "heart" && document.body.className.includes("dark")) {
+                    icon.src = heartBorderWhiteImg;
+                } else {
+                    icon.src = heartImg;
+                }
             }
         });
 
@@ -84,9 +96,194 @@ function styleIcons() {
 }
 
 
+//APPLY DARK MODE
+
+let darkMode = localStorage.getItem("darkMode");
+const toggle = document.getElementById("toggle");
+
+const enableDarkMode = () => {
+    document.body.classList.add("dark");
+
+    localStorage.setItem("darkMode", "enabled")
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("dark");
+
+    localStorage.setItem("darkMode", null)
+}
+
+if (darkMode === "enabled") {
+    enableDarkMode();
+    swapImgOnDarkMode();
+}
+
+toggle.addEventListener("click", () => {
+    console.log(document.body.className);
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "enabled") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+    console.log(document.body.className + "after");
+});
+
+
+//CHANGE IMAGES ON DARKMODE
+
+toggle.addEventListener("click", () => {
+    if (document.body.className.includes("dark")) {
+        swapImgOnDarkMode();
+    } else {
+        swapImgOnLightMode();
+    }
+});
+
+
+function swapImgOnDarkMode() {
+    const images = document.querySelectorAll("img");
+
+    const heartBorderWhiteImg = "../img/heart-outline-white.svg";
+    const whiteEyeImg = "../img/eye-outline-white.svg";
+    const whiteGithubImg = "../img/github-white.svg";
+    const rocket = "../img/rocket-black.svg";
+    const home = "../img/home-black.svg"; 
+    const profile = "../img/profile-black.svg"; 
+    const messages = "../img/messages-black.svg"; 
+    const history = "../img/history-black.svg"; 
+    const tasks = "../img/tasks-black.svg"; 
+    const communities = "../img/communities-black.svg"; 
+    const settings = "../img/settings-black.svg"; 
+    const support = "../img/support-black.svg"; 
+    const privacy = "../img/privacy-black.svg"; 
+    const loupe = "../img/loupe-grey.svg"; 
+    const sun = "../img/sun.svg"; 
+    const moon = "../img/moon-white.svg";
+    const githubBig = "../img/github-white.svg";
+    const handlerRetract = "../img/minimize-black.svg";
+    const handlerExpand = "../img/minimize-flipped-black.svg";    
+    
+    for (const image of images) {
+        if (document.body.className.includes("dark")) {
+            if (image.className.includes("eye")) {
+                image.src = whiteEyeImg;
+            } else if (image.className.includes("github")) {
+                image.src = whiteGithubImg;
+            } else if (image.id === "heart") {
+                image.src = heartBorderWhiteImg;
+            } else if (image.id === "rocket") {
+                image.src = rocket;
+            } else if (image.id === "home") {
+                image.src = home;
+            } else if (image.id === "profile") {
+                image.src = profile;
+            } else if (image.id === "messages") {
+                image.src = messages;
+            } else if (image.id === "history") {
+                image.src = history;
+            } else if (image.id === "tasks") {
+                image.src = tasks;
+            } else if (image.id === "communities") {
+                image.src = communities;
+            } else if (image.id === "settings") {
+                image.src = settings;
+            } else if (image.id === "support") {
+                image.src = support;
+            } else if (image.id === "privacy") {
+                image.src = privacy;
+            } else if (image.id === "loupe") {
+                image.src = loupe;
+            } else if (image.id === "sun") {
+                image.src = sun;
+            } else if (image.id === "moon") {
+                image.src = moon;
+            } else if (image.id === "github-big") {
+                image.src = githubBig;
+            } else if (image.id === "sidebar-handler-retract") {
+                image.src = handlerRetract;
+            } else if (image.id === "sidebar-handler-expand") {
+                image.src = handlerExpand;
+            } 
+        }
+    }
+}
+
+
+function swapImgOnLightMode() {
+    const images = document.querySelectorAll("img");
+
+    const heartBorderWhiteImg = "../img/heart-outline-black.svg";
+    const whiteEyeImg = "../img/eye-outline-black.svg";
+    const whiteGithubImg = "../img/github.svg";
+    const rocket = "../img/rocket.svg";
+    const home = "../img/home.svg"; 
+    const profile = "../img/profile.svg"; 
+    const messages = "../img/messages.svg"; 
+    const history = "../img/history.svg"; 
+    const tasks = "../img/tasks.svg"; 
+    const communities = "../img/communities.svg"; 
+    const settings = "../img/settings.svg"; 
+    const support = "../img/support.svg"; 
+    const privacy = "../img/privacy.svg"; 
+    const loupe = "../img/loupe.svg"; 
+    const sun = "../img/sun-yellow.svg"; 
+    const moon = "../img/moon.svg";
+    const githubBig = "../img/github.svg";
+    const handlerRetract = "../img/minimize.svg";
+    const handlerExpand = "../img/minimize-flipped.svg";    
+    
+    for (const image of images) {
+        if (!document.body.className.includes("dark")) {
+            if (image.className.includes("eye")) {
+                image.src = whiteEyeImg;
+            } else if (image.className.includes("github")) {
+                image.src = whiteGithubImg;
+            } else if (image.id === "heart") {
+                image.src = heartBorderWhiteImg;
+            } else if (image.id === "rocket") {
+                image.src = rocket;
+            } else if (image.id === "home") {
+                image.src = home;
+            } else if (image.id === "profile") {
+                image.src = profile;
+            } else if (image.id === "messages") {
+                image.src = messages;
+            } else if (image.id === "history") {
+                image.src = history;
+            } else if (image.id === "tasks") {
+                image.src = tasks;
+            } else if (image.id === "communities") {
+                image.src = communities;
+            } else if (image.id === "settings") {
+                image.src = settings;
+            } else if (image.id === "support") {
+                image.src = support;
+            } else if (image.id === "privacy") {
+                image.src = privacy;
+            } else if (image.id === "loupe") {
+                image.src = loupe;
+            } else if (image.id === "sun") {
+                image.src = sun;
+            } else if (image.id === "moon") {
+                image.src = moon;
+            } else if (image.id === "github-big") {
+                image.src = githubBig;
+            } else if (image.id === "sidebar-handler-retract") {
+                image.src = handlerRetract;
+            } else if (image.id === "sidebar-handler-expand") {
+                image.src = handlerExpand;
+            } 
+        }
+    }
+}
+    
+
+
+
+
 
 // add shadows to header and main-content sections
 // make the font-sizes responsive
 // increase size for TOP div
-// tweak toggle and add light-dark mode functionality
-// finish interactive elements tweaking
+// use local storage to keep likes after reloding?
